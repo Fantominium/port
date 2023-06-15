805cafff26f1f8db31d2ab05465e8d289bc63002
Malcolm Garner <malcolm.garner4+gitCommitsLocal@gmail.com>
2023-06-15 21:06:35 +0100

    Navbar and theme done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
+import Header from '@/components/Header'
+import Navigation from '@/components/Navigation'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" data-theme="coffee">
       <Head />
       <body>
+        <Navigation/>
         <Main />
         <NextScript />
       </body>
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Blog = (props: Props) => {
+  return (
+    <div>blog</div>
+  )
+}
+
+export default Blog
